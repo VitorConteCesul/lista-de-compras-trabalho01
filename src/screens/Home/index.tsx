5fceddfb7c616dd5e13c435f6b70846ca998947a
VitorConteCesul <vitor.2024014338@alunocesul.com>
2025-09-01 22:03:35 -0300

    style: recondicionamento do item lista de compras

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,3 +1,10 @@
+import { Text, View } from "react-native";
+import { style } from "./styles";
+
 export function Home() {
-  
+  return(
+    <View style={style.container}>
+      <Text style={style.title}>Lista de Compras</Text>
+    </View>
+  )
 }
--- src/screens/Home/styles.tsx
+++ src/screens/Home/styles.tsx
@@ -1,10 +1,23 @@
 import { StyleSheet } from "react-native";
 
 export const style = StyleSheet.create({
- container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  container: {
+    backgroundColor: '#7A4A9E',
+    height: 173,
+    paddingLeft: 24,
+    paddingRight: 24,
     justifyContent: 'center',
+    borderColor: '#ca1a1a',
+    borderStyle: 'solid',
+    borderWidth: 1
+  },
+  title: {
+    color: '#F2F2F2',
+    textAlign: 'center',
+    fontSize: 18,
+    fontFamily: "Inter",
+    fontStyle: "normal",
+    fontWeight: 700,
+    lineHeight: 25.2,
   }
 })
