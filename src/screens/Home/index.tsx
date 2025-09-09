e2c81cc43f9fc042005a40f1ddb4cb7be9387585
VitorConteCesul <vitor.2024014338@alunocesul.com>
2025-09-08 22:37:05 -0300

    feat: customizacao de alguns componentes da tela

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,27 +1,87 @@
-import { FlatList, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Alert, FlatList, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { style } from "./styles";
+import { useState } from "react";
+import { Product } from "../../components/product";
 
 export function Home() {
 
-  function handleAddProduct(){
+  const [produts, setProducts] = useState<string[]>([]);
+  const [productName, setProductName] = useState(" ");
 
+  function handleAddProduct() {
+    if (produts.includes(productName)) {
+      return
+      Alert.alert("Produto já cadastrado", "Já existe um produto na lista com esse nome.");
+    }
+
+    setProducts([...produts, productName])
+    setProductName("")
   }
-  
+
+  function handleProductRemove(name: String) {
+    Alert.alert("Remover", `Deseja remover o produto ${name}?`, [
+      {
+        text: 'Sim',
+        onPress: () => setProducts(produts.filter(product => product !== name))
+      },
+      {
+        text: 'Não',
+        style: 'cancel'
+      }
+    ]);
+  }
+
   return (
     <View style={style.container}>
-      <Text style={style.title}>Lista de Compras</Text>
-{/* 
+      <View style={style.header}>
+        <Text style={style.title}>Lista de Compras</Text>
+      </View>
       <View style={style.form}>
-        <TextInput style={style.input}>
+        <TextInput style={style.input}
           placeholder="Adicione um novo produto"
-        </TextInput>
-      </View> */}
+          placeholderTextColor="#808080"
+        />
+        <TouchableOpacity style={style.button} onPress={handleAddProduct}>
+          <Text>+</Text>
+        </TouchableOpacity>
+      </View>
 
+      <View style={style.label}>
+        <View style={style.textLabel}>
+          <Text style={style.textProduct}>Produtos</Text>
+          <View style={style.sumTotal}>
+            <Text>0</Text>
+          </View>
+        </View>
 
-    <TouchableOpacity style={style.button} onPress={handleAddProduct}>
-      <image style={style.imgButton}
-    </TouchableOpacity>
+        <View style={style.textLabel}>
+          <Text style={style.textFinished}>Finalizados</Text>
+          <View style={style.sumTotal}>
+            <Text>0</Text>
+          </View>
+        </View>
+      </View>
 
+      <View style={style.list}>
+        <FlatList
+          data={produts}
+          keyExtractor={(item) => item}
+          contentContainerStyle={style.list}
+          renderItem={({ item }) => (
+          <Product name={item} onRemove={() => handleProductRemove(item)}/>
+          )}
+          
+          ListEmptyComponent={() => (
+          <Text style={style.listEmpytText}>
+            Você ainda não tem produtos na lista de compra
+            Adicione produtos e organize sua lista de compras
+          </Text>
+        )}
+        />
+      </View>
     </View>
+
+
+    
   )
 }
--- src/screens/Home/styles.tsx
+++ src/screens/Home/styles.tsx
@@ -2,14 +2,18 @@ import { StyleSheet } from "react-native";
 
 export const style = StyleSheet.create({
   container: {
+    flex: 1,
+    backgroundColor: '#F2F2F2'
+  },
+  header: {
     backgroundColor: '#7A4A9E',
     height: 173,
     paddingLeft: 24,
     paddingRight: 24,
     justifyContent: 'center',
-    borderColor: '#ca1a1a',
-    borderStyle: 'solid',
-    borderWidth: 1
+    // borderColor: '#ca1a1a',
+    // borderStyle: 'solid',
+    // borderWidth: 1
   },
   title: {
     color: '#F2F2F2',
@@ -21,22 +25,74 @@ export const style = StyleSheet.create({
     lineHeight: 25.2,
   },
   button: {
-    
+    width: 52,
+    height: 52,
+    borderRadius: 6,
+    backgroundColor: "#31C667",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  form: {
+    position: "relative",
+    top: -25,
+    width: '100%',
+    flexDirection: "row",
+    paddingRight: 24,
+    paddingLeft: 24,
+    gap: 4
+  },
+  input: {
+    flex: 1,
+    height: 54,
+    padding: 16,
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    borderColor: '#808080',
+    borderWidth: 0.5
+  },
+  label: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginLeft: 24,
+    marginRight: 24,
+    paddingBottom: 20,
+    borderColor: '#D9D9D9',
+    borderStyle: 'solid',
+    borderBottomWidth: 1,
+    // borderColor: '#ca1a1a',
+    // borderStyle: 'solid',
+    // borderWidth: 1
+  },
+  textLabel: {
+    flexDirection: 'row'
+  },
+  textProduct: {
+    color: '#31C667',
+    paddingRight: 8
+  },
+  textFinished: {
+    color: '#7A4A9E',
+    paddingRight: 8
+  },
+  sumTotal:{
+    padding: 2,
+    paddingLeft: 8,
+    paddingRight: 8,
+    borderRadius: 999,
+    backgroundColor: '#D9D9D9'
+  },
+
+  list:{
+    flexGrow: 1,
+    justifyContent: 'center'
+
+  },
+  listEmpytText:{
+    fontSize: 16,
+    fontWeight: "normal",
+    lineHeight: 19,
+    textAlign: 'center'
   }
-  // form: {
-  //   width: '100%',
-  //   flexDirection: "row",
-  //   marginTop: 18,
-
-  // },
-  // input: {
-  //   flex: 1,
-  //   height: 56,
-  //   padding: 16,
-  //   backgroundColor: '#ffffff',
-  //   borderRadius: 5,
-  //   marginRight: 16
-  // },
 
 
-})
+});
